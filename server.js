bf18080b0390f913e4264b07e3b993493bb67f08
Mike Hunsicker <hunsickm@matc.edu>
2019-09-19 16:14:41 -0500

    Add mongodb database connection

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import connectDatabase from './config/db';
 
 // Initialize express application
 const app = express();
+
+// Connect database
+connectDatabase();
 
 // API endpoints
 app.get('/', (req, res) =>
