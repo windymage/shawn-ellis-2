081330e3630de57d866e76c4bcb8fd5030e93e14
Mike Hunsicker <hunsickm@matc.edu>
2019-10-02 18:54:03 -0500

    Add user registration endpoint

--- server.js
+++ server.js
@@ -7,10 +7,26 @@ const app = express();
 // Connect database
 connectDatabase();
 
+// Middleware
+app.use(express.json({ extended: false }));
+
 // API endpoints
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
 app.get('/', (req, res) =>
   res.send('http get request sent to root api endpoint')
 );
+
+/**
+ * @route POST api/users
+ * @desc Register user
+ */
+app.post('/api/users', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
 
 // Connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
